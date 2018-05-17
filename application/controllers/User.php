5dc1d8e29175f041be5aaad6d72ea895ca1a83a9
kino <359084415@qq.com>
2018-05-17 19:35:27 +0800

    change header.Utoken into token

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -1,7 +1,7 @@
 <?php
 
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Utoken");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, token");
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 
 defined('BASEPATH') OR exit('No direct script access allowed');
